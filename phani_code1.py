86853db205b6aa0e84613d235001a81098fe4e6a
phani3755 <126799239+phani3755@users.noreply.github.com>
2023-05-12 02:54:48 +0530

    used read file to create data frame

--- phani_code1.py
+++ phani_code1.py
@@ -1,7 +1,97 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 12 02:46:13 2023
+Created on Fri May 12 02:24:11 2023
 
 @author: PHANINDRA SAI NAIDU
 """
 
+# -- coding: utf-8 --
+
+#Importing necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.cluster as cluster
+import sklearn.metrics as skmet
+import scipy.optimize as opt
+
+#Used from class
+import cluster_tools as ct
+import errors as err
+import importlib
+
+
+def data_reading(filename):
+    '''
+    data_reading will create dataframe of gven filename
+
+    Parameters
+    ----------
+    filename : STR
+        File path or location.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        DataFrame created from given filepath.
+
+    '''
+    df = pd.read_csv(filename, skiprows=4)
+    df = df.set_index('Country Name', drop=True)
+    df = df.loc[:, '1960':'2021']
+
+    return df
+
+
+def data_transpose(df):
+    '''
+    data_transpose creates an new dataframe, transpose of given dataframe
+
+    Parameters
+    ----------
+    df  : pandas.DataFrame
+        DataFrame for which transpose to be found.
+
+    Returns
+    -------
+    data_tr : pandas.DataFrame
+        Transposed DataFrame of given DataFrame.
+
+    '''
+    df_tr = df.transpose()
+
+    return df_tr
+
+
+def correlation_and_scattermatrix(df):
+    '''
+    correlation_and_scattermatrix plots correlation matrix and scatter plots
+    of data among columns
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame for which analysis will be done.
+
+    Returns
+    -------
+    None.
+
+    '''
+    corr = df.corr()
+    print(corr)
+    plt.figure(figsize=(10, 10))
+    plt.matshow(corr, cmap='coolwarm')
+
+    # xticks and yticks for corr matrix
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.title(
+        'Electricity power consumption kWh per capita')
+    plt.colorbar()
+    plt.show()
+
+    pd.plotting.scatter_matrix(df, figsize=(12, 12), s=5, alpha=0.8)
+    plt.show()
+
+    return
